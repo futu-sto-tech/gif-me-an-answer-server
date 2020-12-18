ea398581372f51c2d782b50a05eacd2c81407af1
Jens Östlund <jens.ostlund@futurice.com>
2020-12-18 09:36:38 +0100

    Add cors headers

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
 import express from 'express';
+import cors from 'cors';
 
 const port = process.env.PORT || 8000;
 
 const app = express();
+app.use(cors());
 
 app.get('/', (_req, res) => {
   res.json({ message: "I'm alive!!!" });
