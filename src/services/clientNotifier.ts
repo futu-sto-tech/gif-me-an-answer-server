5af377093f086c8613ea75190c39a37e88a44146
Jens Östlund <jens.ostlund@futurice.com>
2021-03-14 15:40:50 +0100

    Add support for Redis-backed client notifications
    By using Redis pubsub capabilities we can keep client notifications in
    sync across multiple servers which allows us to scale horizontally. If
    `REDIS_URL` is not set it will fallback to an EventEmitter based
    pubsub implementation which works if running on a single server.

--- src/services/clientNotifier.ts
+++ src/services/clientNotifier.ts
@@ -1,5 +1,8 @@
+import { EventEmitter } from 'events';
 import { Response } from 'express';
+import { createClient, RedisClient } from 'redis';
 import { v4 as uuidv4 } from 'uuid';
+import config from '../config';
 import logger from '../logger';
 import { Events } from '../types';
 
@@ -9,11 +12,84 @@ interface Client {
   res: Response;
 }
 
+interface PubSub {
+  onMessage: (cb: (gameCode: number, eventName: Events, data: any) => void) => void;
+  publishMessage: (gameCode: number, eventName: Events, data: any) => void;
+}
+
+class EventPubSub implements PubSub {
+  private emitter: EventEmitter;
+
+  constructor() {
+    this.emitter = new EventEmitter();
+  }
+
+  onMessage = (cb: (gameCode: number, eventName: Events, data: any) => void) => {
+    this.emitter.on('message', cb);
+  };
+
+  publishMessage = (gameCode: number, eventName: Events, data: any) => {
+    this.emitter.emit('message', gameCode, eventName, data);
+  };
+}
+
+class RedisPubSub implements PubSub {
+  private channel = 'gmaa-game-events';
+  private subscriber: RedisClient;
+  private publisher: RedisClient;
+
+  constructor(url: string) {
+    this.publisher = createClient(url);
+    this.subscriber = createClient(url);
+    this.subscriber.subscribe(this.channel);
+  }
+
+  onMessage = (cb: (gameCode: number, eventName: Events, data: any) => void) => {
+    this.subscriber.on('message', (_channel, message) => {
+      const { gameCode, eventName, data } = JSON.parse(message) as { gameCode: number; eventName: Events; data: any };
+
+      cb(gameCode, eventName, data);
+    });
+  };
+
+  publishMessage = (gameCode: number, eventName: Events, data: any) => {
+    this.publisher.publish(this.channel, JSON.stringify({ gameCode, eventName, data }));
+  };
+}
+
 export class ClientNotifier {
   private clients: Client[];
+  private subscriber: PubSub;
+  private publisher: PubSub;
 
   constructor() {
     this.clients = [];
+
+    if (config.REDIS_URL) {
+      logger.info({ message: 'Initializing ClientNotifier with RedisPubSub' });
+
+      this.subscriber = new RedisPubSub(config.REDIS_URL);
+      this.publisher = new RedisPubSub(config.REDIS_URL);
+    } else {
+      logger.info({ message: 'Initializing ClientNotifier with EventPubSub' });
+
+      const pubSub = new EventPubSub();
+      this.subscriber = pubSub;
+      this.publisher = pubSub;
+    }
+
+    this.subscriber.onMessage((gameCode, eventName, data) => {
+      const players = this.clients.filter((c) => c.gameCode === gameCode);
+
+      const payload = {
+        event: eventName,
+        data,
+      };
+
+      players.forEach((p) => {
+        p.res.write(`data: ${JSON.stringify(payload)}\n\n`);
+      });
+    });
   }
 
   addClient(gameCode: number, res: Response): string {
@@ -45,15 +121,6 @@ export class ClientNotifier {
   }
 
   notifyGameClients<T>(gameCode: number, eventName: Events, data: Exclude<T, Promise<any>>) {
-    const players = this.clients.filter((c) => c.gameCode === gameCode);
-
-    const payload = {
-      event: eventName,
-      data,
-    };
-
-    players.forEach((p) => {
-      p.res.write(`data: ${JSON.stringify(payload)}\n\n`);
-    });
+    this.publisher.publishMessage(gameCode, eventName, data);
   }
 }
